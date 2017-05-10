2dd9874e64aee62f4f8c3cc5d9b39052239e851d
dam <puchadesivan@gmail.com>
2017-05-10 18:46:27 +0200

    Añadido Menú y link de menu(Torneo), a la clase Torneo

--- app/src/main/java/com/bisai/bisai/controller/activities/main/MainActivity.java
+++ app/src/main/java/com/bisai/bisai/controller/activities/main/MainActivity.java
@@ -1,15 +1,18 @@
 package com.bisai.bisai.controller.activities.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.bisai.bisai.R;
 
 public class MainActivity extends AppCompatActivity {
-    private MenuItem sel24hMenuItem;
+    private MenuItem menuEquipoTorneo;
+    ImageButton masterkey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
-        sel24hMenuItem = menu.findItem(R.id.perfil);
+        menuEquipoTorneo = menu.findItem(R.id.torneo);
         return true;
 
     }
@@ -28,14 +31,24 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.perfil:
 
-
             case R.id.torneo:
-
+                    Intent masterkey = new Intent(MainActivity.this, TorneoListActivity.class);
+                    startActivity(masterkey);
 
             case R.id.equipo:
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeEquipos(boolean selEquipos){
+        if (selEquipos){
+
+        }
+        else {
+
+        }
+        //this.selEquipos = selEquipos;
     }
 
 
---
+++ app/src/main/java/com/bisai/bisai/controller/activities/main/TorneoListActivity.java
@@ -0,0 +1,8 @@
+package com.bisai.bisai.controller.activities.main;
+
+/**
+ * Created by dam on 10/5/17.
+ */
+
+public class TorneoListActivity {
+}
