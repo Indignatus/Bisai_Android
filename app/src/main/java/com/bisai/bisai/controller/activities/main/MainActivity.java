8112ba249a752fc7ae00fb43f24f9c72f81d0847
Sergi Tirado <sergi2496@hotmail.com>
2017-06-02 19:40:19 +0200

    Intento push

--- app/src/main/java/com/bisai/bisai/controller/activities/main/MainActivity.java
+++ app/src/main/java/com/bisai/bisai/controller/activities/main/MainActivity.java
@@ -17,43 +17,7 @@ import com.bisai.bisai.controller.managers.UserLoginManager;
 public class MainActivity extends AppCompatActivity {
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-                button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                               Intent i = new Intent(MainActivity.this, TeamListActivity.class);
-                                startActivity(i);
-                            }
-                  });
-
-        Button button2 = (Button) findViewById(R.id.crearEquipo);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, AddPlayerActivity.class);
-                startActivity(i);
-            }
-        });
-
-        Button button3 = (Button) findViewById(R.id.verEquiposDeJugador);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, TeamPlayerListActivity.class);
-                startActivity(i);
-            }
-        });
-
-        TextView nikname = (TextView) findViewById(R.id.nikname);
-
-        // sigleton como es 1 en toda la aplicación sobre el UserLoginMaeger tenemos que hacer un getInstance
-        nikname.setText(UserLoginManager.getInstance().getJugador().getNickName());
-    }
 
 
 }
